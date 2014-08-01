209efa3cf8c937d03c2f456a587c89fa1646bc42
lantran <lantran@terpmail.umd.edu>
2014-08-01 16:36:44 -0400

    Finish

--- SimpleDatabase1.java
+++ SimpleDatabase1.java
@@ -4,12 +4,14 @@ import java.util.*;
 
 public class SimpleDatabase1 extends ConsoleProgram{
 
-	Map<String,String> currentTransaction = new HashMap<String,String>();
-	Stack<Map> blocks = new Stack<Map>();
-
+	Map<String,String> currentTransaction = new HashMap<String,String>(); 
+	Stack<Map<String,String>> blocks = new Stack<Map<String,String>>();
 	public void run(){
 		String cmd;
+		//int index = -1;
 		do{
+
+
 			cmd = readLine();
 			StringTokenizer tokenizer = new StringTokenizer(cmd);
 			ArrayList<String> components = new ArrayList<String>();
@@ -17,6 +19,52 @@ public class SimpleDatabase1 extends ConsoleProgram{
 			for (int i = 0 ; tokenizer.hasMoreTokens() ; i++){
 				components.add(tokenizer.nextToken());
 			}
+
+
+
+			if (components.get(0).equals("BEGIN")){
+				//index++;
+				Map<String,String> newTransaction = new HashMap<String,String>();
+				if (currentTransaction.size()!= 0){
+					for (String name : currentTransaction.keySet()){
+						newTransaction.put(name, currentTransaction.get(name));
+					}
+				}
+				
+				
+				blocks.push(newTransaction);
+				currentTransaction = blocks.peek();
+			}else if(components.get(0).equals("ROLLBACK")){
+				if (!blocks.isEmpty()){ // escape from current block 
+					//println(currentTransaction);
+					blocks.pop();
+					if (!blocks.isEmpty()){ 
+						currentTransaction = blocks.peek();
+						//println("DONE");
+						//println(currentTransaction);
+					}else{
+						currentTransaction = null;
+						println("NO TRANSACTION");
+					}
+				}else{
+					println("NO TRANSACTION");
+				}
+				// currentTransaction is the one right after that
+
+			}else if (components.get(0).equals("COMMIT")){
+				if(!blocks.isEmpty()){
+					currentTransaction = blocks.peek();
+					
+				}
+				// blocks = null; cannot erase a Stack by assign a NULL value
+				
+				while (!blocks.isEmpty()){
+					blocks.pop();
+				}
+				blocks.push(currentTransaction);
+			}
+
+
 
 			if(components.get(0).equals("SET")){
 				setMethod(components );
@@ -41,9 +89,13 @@ public class SimpleDatabase1 extends ConsoleProgram{
 
 	private void getMethod(ArrayList<String> components){
 		name = components.get(1);
-		if(currentTransaction.containsKey(name)){
-			println(currentTransaction.get(name));
-		}else{
+		try{
+			if(currentTransaction.containsKey(name)){
+				println(currentTransaction.get(name));
+			}else{
+				println("NULL");
+			}
+		}catch (NullPointerException e){
 			println("NULL");
 		}
 	}
